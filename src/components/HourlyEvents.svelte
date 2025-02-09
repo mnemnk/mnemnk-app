ff636a098a945068e9ebd4877fc978c868b5d904
Akira Ishino <akira@lumilab.jp>
2025-02-09 12:29:35 +0900

    feat: 🎸 support more frequent app updates (#34)

--- src/components/HourlyEvents.svelte
+++ src/components/HourlyEvents.svelte
@@ -23,16 +23,16 @@
   //   [...new Set(events.map((ev) => new Date(ev.time).getHours()))].sort((a, b) => b - a),
   // );
 
-  const APPS_WINDOW_SIZE = 60;
-  const NUM_TRACKING_APPS = 4;
+  // const APPS_WINDOW_SIZE = 60;
+  // const NUM_TRACKING_APPS = 4;
 
-  // [dt, screenshot, application, previous applications, others]
-  type EventRow = [number, MnemnkEvent | null, MnemnkEvent | null, string[], MnemnkEvent[]];
+  // [dt, screenshot, applications, previous applications, others]
+  type EventRow = [number, MnemnkEvent | null, MnemnkEvent[], MnemnkEvent[]];
   let events_apps: [Map<number, EventRow>, Set<string>] = $derived.by(() => {
     let events_by_dt = new Map<number, EventRow>();
     let last_app = "";
-    let apps = [] as string[];
-    let apps_window = [] as string[];
+    // let apps = [] as string[];
+    // let apps_window = [] as string[];
     let all_apps = new Set() as Set<string>;
     events.forEach((ev) => {
       let d = new Date(ev.time);
@@ -44,27 +44,26 @@
         d.getMinutes();
       let event = events_by_dt.get(dt);
       if (!event) {
-        apps_window = [last_app, ...apps_window].slice(0, APPS_WINDOW_SIZE);
-        apps = apps.filter((a) => apps_window.includes(a));
-        event = [dt, null, null, apps, []] as EventRow;
+        // apps_window = [last_app, ...apps_window].slice(0, APPS_WINDOW_SIZE);
+        // apps = apps.filter((a) => apps_window.includes(a));
+        event = [dt, null, [], []] as EventRow;
       }
       if (ev.kind === "screen") {
         event[1] = ev;
       } else if (ev.kind === "application") {
-        event[2] = ev;
         last_app = ev.value.name;
-        apps = apps.includes(last_app)
-          ? [last_app, ...apps.filter((a) => a !== last_app)]
-          : [last_app, ...apps].slice(0, NUM_TRACKING_APPS);
-        apps = apps.slice(0, NUM_TRACKING_APPS);
-        event[3] = apps;
+        // apps = apps.includes(last_app)
+        //   ? [last_app, ...apps.filter((a) => a !== last_app)]
+        //   : [last_app, ...apps].slice(0, NUM_TRACKING_APPS);
+        // apps = apps.slice(0, NUM_TRACKING_APPS);
+        event[2].unshift(ev);
         all_apps.add(last_app);
       } else {
         if (ev.kind === "browser") {
           let url = new URL(ev.value.url);
           ev.value.hostname = url.hostname;
         }
-        event[4].push(ev);
+        event[3].unshift(ev);
       }
       events_by_dt.set(dt, event);
     });
@@ -76,68 +75,68 @@
 
   // $inspect(events_by_dt);
 
-  const ROW_HEIGHT = 96;
-  const ROW_OFFSET = 16;
-  const COL_WIDTH = 100;
-  const NODE_HEIGHT = 15;
+  // const ROW_HEIGHT = 96;
+  // const ROW_OFFSET = 16;
+  // const COL_WIDTH = 100;
+  // const NODE_HEIGHT = 15;
 
-  const APP_COLOR_PALLETE = ["#FFF100", "#FF4B00", "#005AFF", "#03AF7A", "#F6AA00", "#4DC4FF"];
+  // const APP_COLOR_PALLETE = ["#FFF100", "#FF4B00", "#005AFF", "#03AF7A", "#F6AA00", "#4DC4FF"];
 
-  let app_colors: Record<string, string> = $derived.by(() => {
-    const colors = {} as Record<string, string>;
-    [...all_apps].forEach((app, i) => {
-      colors[app] = APP_COLOR_PALLETE[i % APP_COLOR_PALLETE.length];
-    });
-    return colors;
-  });
+  // let app_colors: Record<string, string> = $derived.by(() => {
+  //   const colors = {} as Record<string, string>;
+  //   [...all_apps].forEach((app, i) => {
+  //     colors[app] = APP_COLOR_PALLETE[i % APP_COLOR_PALLETE.length];
+  //   });
+  //   return colors;
+  // });
 
-  let app_paths = $derived.by(() => {
-    const paths = [] as { path: string; color: string }[];
-    const appPositions = {} as Record<string, { row: number; col: number }[]>;
+  // let app_paths = $derived.by(() => {
+  //   const paths = [] as { path: string; color: string }[];
+  //   const appPositions = {} as Record<string, { row: number; col: number }[]>;
 
-    let num_events = events_by_dt.length;
-    events_by_dt.forEach((row, rowIndex) => {
-      row[3].forEach((app, colIndex) => {
-        if (!appPositions[app]) {
-          appPositions[app] = [];
-        }
-        appPositions[app].push({ row: num_events - rowIndex - 1, col: colIndex });
-      });
-    });
+  //   let num_events = events_by_dt.length;
+  //   events_by_dt.forEach((row, rowIndex) => {
+  //     row[2][1].forEach((app, colIndex) => {
+  //       if (!appPositions[app]) {
+  //         appPositions[app] = [];
+  //       }
+  //       appPositions[app].push({ row: num_events - rowIndex - 1, col: colIndex });
+  //     });
+  //   });
 
-    Object.entries(appPositions).forEach(([app, positions]) => {
-      if (positions.length === 1) {
-        const x = positions[0].col * COL_WIDTH + COL_WIDTH / 2;
-        const y = positions[0].row * ROW_HEIGHT + ROW_OFFSET;
-        paths.push({
-          path: `M ${x} ${y + NODE_HEIGHT} L ${x} ${y - NODE_HEIGHT / 2}`,
-          color: app_colors[app],
-        });
-      }
-      const path = positions.reduce((acc, pos, i, arr) => {
-        const x = pos.col * COL_WIDTH + COL_WIDTH / 2;
-        const y = pos.row * ROW_HEIGHT + ROW_OFFSET;
+  //   Object.entries(appPositions).forEach(([app, positions]) => {
+  //     if (positions.length === 1) {
+  //       const x = positions[0].col * COL_WIDTH + COL_WIDTH / 2;
+  //       const y = positions[0].row * ROW_HEIGHT + ROW_OFFSET;
+  //       paths.push({
+  //         path: `M ${x} ${y + NODE_HEIGHT} L ${x} ${y - NODE_HEIGHT / 2}`,
+  //         color: app_colors[app],
+  //       });
+  //     }
+  //     const path = positions.reduce((acc, pos, i, arr) => {
+  //       const x = pos.col * COL_WIDTH + COL_WIDTH / 2;
+  //       const y = pos.row * ROW_HEIGHT + ROW_OFFSET;
 
-        if (i === 0) return `M ${x} ${y + NODE_HEIGHT}`;
+  //       if (i === 0) return `M ${x} ${y + NODE_HEIGHT}`;
 
-        const prevX = arr[i - 1].col * COL_WIDTH + COL_WIDTH / 2;
-        const prevY = arr[i - 1].row * ROW_HEIGHT + ROW_OFFSET;
+  //       const prevX = arr[i - 1].col * COL_WIDTH + COL_WIDTH / 2;
+  //       const prevY = arr[i - 1].row * ROW_HEIGHT + ROW_OFFSET;
 
-        if (arr[i - 1].row - 1 !== pos.row) {
-          return acc + ` L ${prevX} ${prevY - NODE_HEIGHT / 2} M ${x} ${y + NODE_HEIGHT / 2}`;
-        }
+  //       if (arr[i - 1].row - 1 !== pos.row) {
+  //         return acc + ` L ${prevX} ${prevY - NODE_HEIGHT / 2} M ${x} ${y + NODE_HEIGHT / 2}`;
+  //       }
 
-        const startY = prevY - NODE_HEIGHT / 2;
-        const endY = y - NODE_HEIGHT / 2;
-        const controlY = (startY + endY) / 2;
-        return acc + ` L ${prevX} ${startY} C ${prevX} ${controlY}, ${x} ${controlY}, ${x} ${endY}`;
-      }, "");
+  //       const startY = prevY - NODE_HEIGHT / 2;
+  //       const endY = y - NODE_HEIGHT / 2;
+  //       const controlY = (startY + endY) / 2;
+  //       return acc + ` L ${prevX} ${startY} C ${prevX} ${controlY}, ${x} ${controlY}, ${x} ${endY}`;
+  //     }, "");
 
-      paths.push({ path, color: app_colors[app] });
-    });
+  //     paths.push({ path, color: app_colors[app] });
+  //   });
 
-    return paths;
-  });
+  //   return paths;
+  // });
 
   // $inspect(app_paths);
 
@@ -185,7 +184,7 @@
     </div>
   {:else}
     <div class="w-full overflow-x-auto relative">
-      <div class="pointer-events-none absolute top-0 left-36">
+      <!-- <div class="pointer-events-none absolute top-0 left-36">
         <svg width={COL_WIDTH * NUM_TRACKING_APPS} height={ROW_HEIGHT * events_by_dt.length}>
           {#each app_paths as pathData, i}
             <path
@@ -197,7 +196,7 @@
             />
           {/each}
         </svg>
-      </div>
+      </div> -->
 
       <div class="ml-4">
         <!-- <h1 class="text-3xl font-bold bg-transparent/60 pt-2 pb-4">{date}</h1> -->
@@ -229,27 +228,27 @@
               {/if}
             </div>
             <div class="flex-none w-1/2 overflow-hidden mr-4">
-              {#if row[2]}
-                <div id="e-{row[2].id.id.String}" class="text-nowrap">
-                  {row[2].value.title}
+              {#each row[2] as app (app.id.id.String)}
+                <div id="e-{app.id.id.String}" class="text-nowrap">
+                  {app.value.title}
                 </div>
                 <Popover
                   arrow={false}
                   offset={1}
                   placement="bottom-start"
                   trigger="click"
-                  triggeredBy="#e-{row[2].id.id.String}"
+                  triggeredBy="#e-{app.id.id.String}"
                   class="!text-primary-50 !bg-gray-700"
                 >
                   <div class="p-3">
-                    <div>{row[2].time.toLocaleString()}</div>
-                    <pre>{JSON.stringify(row[2].value, null, 2)}</pre>
+                    <div>{app.time.toLocaleString()}</div>
+                    <pre>{JSON.stringify(app.value, null, 2)}</pre>
                   </div>
                 </Popover>
-              {/if}
+              {/each}
             </div>
             <div class="flex-auto w-96 text-nowrap">
-              {#each row[4] as event (event.id.id.String)}
+              {#each row[3] as event (event.id.id.String)}
                 {#if event.kind === "browser"}
                   <div id="e-{event.id.id.String}">
                     <img
