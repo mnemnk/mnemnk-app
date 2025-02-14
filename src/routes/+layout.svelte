c481f517ad5998b2faa2bf40e31b16264565fbdb
Akira Ishino <akira@lumilab.jp>
2025-02-14 20:33:45 +0900

    feat: 🎸 close window using escape
    ✅ Closes: #51

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,25 @@
-<script>
+<script lang="ts">
+  import { getCurrentWindow } from '@tauri-apps/api/window';
+
   import Attribution from "@/components/Attribution.svelte";
 
   import "../app.css";
 
   /** @type {{children?: import('svelte').Snippet}} */
   let { children } = $props();
+
+  async function on_keydown(event: KeyboardEvent) {
+    if (event.repeat) {
+      return;
+    }
+    if (event.key === "Escape") {
+      event.preventDefault();
+      await getCurrentWindow().close();
+    }
+  }
 </script>
 
 {@render children?.()}
 <Attribution />
+
+<svelte:window on:keydown={on_keydown} />
