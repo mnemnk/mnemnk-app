5a58ca9c3b07748bd4753d4166690814f3f8fa53
Akira Ishino <akira@lumilab.jp>
2025-02-17 21:45:57 +0900

    style: 💄 change settings icon (#58)

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,6 +1,6 @@
 <script>
   import { BottomNav, BottomNavItem, Tooltip } from "flowbite-svelte";
-  import { HomeSolid, AdjustmentsVerticalOutline, SearchSolid } from "flowbite-svelte-icons";
+  import { HomeSolid, CogOutline, SearchSolid } from "flowbite-svelte-icons";
 
   import { page } from "$app/state";
 
@@ -28,7 +28,7 @@
     <Tooltip arrow={false}>Search</Tooltip>
   </BottomNavItem>
   <BottomNavItem btnName="Settings" href="/settings" appBtnPosition="right">
-    <AdjustmentsVerticalOutline
+    <CogOutline
       class="w-4 h-4 mb-1/2 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500"
     />
     <Tooltip arrow={false}>Settings</Tooltip>
