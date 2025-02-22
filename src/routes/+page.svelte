2dee300d5d954acf87775c31a3daf7a3029db19f
Akira Ishino <akira@lumilab.jp>
2025-02-23 01:38:13 +0900

    refactor: 💡 use route params for daily path (#70)
    https://svelte.dev/docs/kit/load#Using-URL-data-params

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,9 +11,7 @@
 
   function onDateChange(date: string) {
     let d = new Date(date);
-    // date string YYYYMMDD
-    // let date_str = `${d.getFullYear()}${(d.getMonth() + 1).toString().padStart(2, '0')}${d.getDate().toString().padStart(2, '0')}`;
-    goto(`/daily?d=${dateString(d)}`);
+    goto(`/daily/${dateString(d)}/`);
   }
 </script>
 
