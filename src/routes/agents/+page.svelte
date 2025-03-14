40d70a4b0df17e754d1250114cacaa89b79da0f2
Akira Ishino <akira@lumilab.jp>
2025-03-14 18:03:16 +0900

    feat: 🎸 add board node

--- src/routes/agents/+page.svelte
+++ src/routes/agents/+page.svelte
@@ -1,23 +1,17 @@
 <script lang="ts">
-  import { get, writable } from "svelte/store";
+  import { writable } from "svelte/store";
   import type { Writable } from "svelte/store";
 
   import { SvelteFlow, Controls, type NodeTypes } from "@xyflow/svelte";
   // 👇 this is important! You need to import the styles for Svelte Flow to work
   import "@xyflow/svelte/dist/style.css";
-  import { Button, Drawer, GradientButton } from "flowbite-svelte";
-  import { nanoid } from "nanoid";
 
-  import {
-    deserializeAgentFlow,
-    deserializeAgentFlowNode,
-    save_agent_flow,
-    serializeAgentFlow,
-    setAgentSettingsContext,
-  } from "@/lib/agent";
+  import { deserializeAgentFlow, setAgentSettingsContext } from "@/lib/agent";
   import type { SAgentNode } from "@/lib/types";
 
+  import AgentDrawer from "./AgentDrawer.svelte";
   import AgentNode from "./AgentNode.svelte";
+  import BoardNode from "./BoardNode.svelte";
 
   const { data } = $props();
 
@@ -28,6 +22,7 @@
   const edges = writable([]);
   const nodeTypes: NodeTypes = {
     agent: AgentNode,
+    board: BoardNode,
   };
 
   const flow_index = $state(0);
@@ -35,29 +30,6 @@
   $effect(() => {
     nodes.set(deserializeAgentFlow(data.agent_flows[flow_index], data.settings));
   });
-
-  function addNode(agent_name: string) {
-    const id = nanoid();
-    const default_config = data.settings[agent_name]?.default_config || {};
-    const node_data = {
-      id,
-      name: agent_name,
-      enabled: true,
-      config: {
-        ...default_config,
-      },
-      x: Math.random() * 1000,
-      y: Math.random() * 1000,
-    };
-    nodes.update((nodes) => {
-      return [...nodes, deserializeAgentFlowNode(node_data, data.settings)];
-    });
-  }
-
-  async function updateAgentFlow() {
-    const flow = serializeAgentFlow(get(nodes));
-    await save_agent_flow(flow, flow_index);
-  }
 </script>
 
 <main class="container static min-w-[100vw]">
@@ -73,22 +45,7 @@
     <Controls />
   </SvelteFlow>
 
-  <Drawer
-    activateClickOutside={false}
-    backdrop={false}
-    hidden={false}
-    placement="right"
-    class="w-200"
-  >
-    <GradientButton color="pinkToOrange" class="w-full mb-4" onclick={updateAgentFlow}>
-      Update
-    </GradientButton>
-    {#each catalog as agent}
-      <div class="mb-4">
-        <Button class="w-full" outline onclick={() => addNode(agent.name)}>{agent.name}</Button>
-      </div>
-    {/each}
-  </Drawer>
+  <AgentDrawer {catalog} {flow_index} {nodes} settings={data.settings} />
 </main>
 
 <style>
--- src/routes/agents/AgentNode.svelte
+++ src/routes/agents/AgentNode.svelte
@@ -14,7 +14,7 @@
     data: {
       name: string;
       enabled: Writable<boolean>;
-      config: Record<string, AgentConfig> | null;
+      config: Record<string, AgentConfig>;
     };
   };
 
@@ -41,28 +41,26 @@
     <form class="grid grid-cols-6 gap-4 p-4">
       <Toggle bind:checked={() => get(data.enabled), (v) => data.enabled.set(v)} class="col-span-6"
       ></Toggle>
-      {#if data.config}
-        {#each Object.keys(data.config) as key}
-          {@const config = data.config[key]}
-          <Label class="col-span-6 space-y-2">
-            <h3>{config.title || key}</h3>
-            <p class="text-xs text-gray-500">{config["description"]}</p>
-            {#if config.type === "boolean"}
-              <Toggle bind:checked={() => get(config.value), (v) => config.value.set(v)} />
-            {:else if config.type === "integer"}
-              <NumberInput bind:value={() => get(config.value), (v) => config.value.set(v)} />
-            {:else if config.type === "number"}
-              <Input type="text" bind:value={() => get(config.value), (v) => config.value.set(v)} />
-            {:else if config.type === "string" || config.type === "string?"}
-              <Input type="text" bind:value={() => get(config.value), (v) => config.value.set(v)} />
-            {:else if config.type === "string[]"}
-              <Textarea bind:value={() => get(config.value), (v) => config.value.set(v)} rows={4} />
-            {:else}
-              <Input type="text" value={get(config.value)} disabled />
-            {/if}
-          </Label>
-        {/each}
-      {/if}
+      {#each Object.keys(data.config) as key}
+        {@const config = data.config[key]}
+        <Label class="col-span-6 space-y-2">
+          <h3>{config.title || key}</h3>
+          <p class="text-xs text-gray-500">{config["description"]}</p>
+          {#if config.type === "boolean"}
+            <Toggle bind:checked={() => get(config.value), (v) => config.value.set(v)} />
+          {:else if config.type === "integer"}
+            <NumberInput bind:value={() => get(config.value), (v) => config.value.set(v)} />
+          {:else if config.type === "number"}
+            <Input type="text" bind:value={() => get(config.value), (v) => config.value.set(v)} />
+          {:else if config.type === "string" || config.type === "string?"}
+            <Input type="text" bind:value={() => get(config.value), (v) => config.value.set(v)} />
+          {:else if config.type === "string[]"}
+            <Textarea bind:value={() => get(config.value), (v) => config.value.set(v)} rows={4} />
+          {:else}
+            <Input type="text" value={get(config.value)} disabled />
+          {/if}
+        </Label>
+      {/each}
     </form>
   </Card>
 </div>
