15f2b85c781e498cf15b344f449db095698ca52b
Akira Ishino <akira@lumilab.jp>
2025-03-14 16:13:02 +0900

    fix: 🐛 save and restore agents (#95)
    * fix: 🐛 save and restore agents

---
+++ src/lib/agent.ts
@@ -0,0 +1,227 @@
+import { invoke } from "@tauri-apps/api/core";
+
+import { getContext, setContext } from "svelte";
+import { get, writable } from "svelte/store";
+
+import type {
+  AgentCatalogEntry,
+  AgentConfig,
+  AgentDefaultConfig,
+  AgentFlow,
+  AgentFlowNode,
+  AgentSchema,
+  AgentSettings,
+  SAgentNode,
+} from "./types";
+
+export async function get_agent_catalog(): Promise<AgentCatalogEntry[]> {
+  return await invoke("get_agent_catalog_cmd");
+}
+
+export async function start_agent(agent_id: string): Promise<void> {
+  await invoke("start_agent_cmd", { agent_id });
+}
+
+export async function stop_agent(agent_id: string): Promise<void> {
+  await invoke("stop_agent_cmd", { agent_id });
+}
+
+export async function save_agent_config(
+  agent_id: string,
+  config: Record<string, any>,
+): Promise<void> {
+  await invoke("save_agent_config_cmd", { agent_id, config });
+}
+
+export async function set_agent_enabled(agent_id: string, enabled: boolean): Promise<void> {
+  await invoke("set_agent_enabled_cmd", { agent_id, enabled });
+}
+
+export async function get_agent_settings(): Promise<Record<string, AgentSettings>> {
+  return await invoke("get_agent_settings_cmd");
+}
+
+export async function get_agent_flows(): Promise<AgentFlow[]> {
+  return await invoke("get_agent_flows_cmd");
+}
+
+export async function save_agent_flow(agent_flow: AgentFlow, idx: number): Promise<void> {
+  await invoke("save_agent_flow_cmd", { agent_flow, idx });
+}
+
+const agentSettingsKey = Symbol("agentSettings");
+
+export function setAgentSettingsContext(settings: Record<string, AgentSettings>): void {
+  setContext(agentSettingsKey, settings);
+}
+
+export function getAgentSettingsContext(): Record<string, AgentSettings> {
+  return getContext(agentSettingsKey);
+}
+
+// Agent Flow
+
+// deserialize: AgentFlow -> SAgentNode[]
+
+export function deserializeAgentFlows(
+  flows: AgentFlow[],
+  agent_settings: Record<string, AgentSettings>,
+): SAgentNode[][] {
+  return flows.map((flow) => deserializeAgentFlow(flow, agent_settings));
+}
+
+export function deserializeAgentFlow(
+  flow: AgentFlow,
+  agent_settings: Record<string, AgentSettings>,
+): SAgentNode[] {
+  return flow.nodes.map((node) => deserializeAgentFlowNode(node, agent_settings));
+}
+
+export function deserializeAgentFlowNode(
+  node: AgentFlowNode,
+  agent_settings: Record<string, AgentSettings>,
+): SAgentNode {
+  const settings = agent_settings[node.name];
+  const default_config = settings?.default_config;
+  const schema_properties = settings?.schema?.properties;
+  return {
+    id: node.id,
+    type: "agent",
+    data: {
+      name: node.name,
+      enabled: writable(node.enabled),
+      config: deserializeAgentFlowNodeConfig(node.config, default_config, schema_properties),
+    },
+    position: {
+      x: node.x,
+      y: node.y,
+    },
+    width: node.width,
+    height: node.height,
+  };
+}
+
+function deserializeAgentFlowNodeConfig(
+  node_config: Record<string, any> | null,
+  default_config: AgentDefaultConfig | null,
+  schema_properties: AgentSchema | null,
+): Record<string, AgentConfig> {
+  if (!node_config) {
+    node_config = {};
+  }
+  if (default_config) {
+    for (const key of Object.keys(default_config)) {
+      if (node_config[key] === undefined) {
+        node_config[key] = default_config[key];
+      }
+    }
+  }
+  const agent_config: Record<string, AgentConfig> = {};
+  for (const key of Object.keys(node_config)) {
+    const config: AgentConfig = {
+      value: writable(null),
+      type: null,
+      title: null,
+      description: null,
+    };
+    const property = schema_properties?.[key];
+    if (property) {
+      const t = property.type;
+      if (t === "boolean") {
+        config.type = "boolean";
+        config.value = writable(node_config[key]);
+      } else if (t === "integer") {
+        config.type = "integer";
+        config.value = writable(node_config[key]);
+      } else if (t === "number") {
+        config.type = "number";
+        config.value = writable(node_config[key].toString());
+      } else if (t === "string") {
+        config.type = "string";
+        config.value = writable(node_config[key]);
+      } else if (isOptionString(t)) {
+        config.type = "string?";
+        config.value = writable(node_config[key] === null ? "" : node_config[key]);
+      } else if (isArrayString(t, property.items?.type)) {
+        config.type = "string[]";
+        config.value = writable((node_config[key] as string[]).join("\n"));
+      } else {
+        config.type = `unknown (${t})`;
+        config.value = writable(node_config[key]);
+      }
+      if (property.title) {
+        config.title = property.title;
+      }
+      if (property.description) {
+        config.description = property.description;
+      }
+    } else {
+      config.type = "unknown";
+      config.value = writable(node_config[key]);
+    }
+    agent_config[key] = config;
+  }
+  return agent_config;
+}
+
+function isOptionString(t: any): boolean {
+  return Array.isArray(t) && t.length === 2 && t.includes("string") && t.includes("null");
+}
+
+function isArrayString(t: any, items_t: any): boolean {
+  return t === "array" && items_t === "string";
+}
+
+// serialize: SAgentNode[] -> AgentFlow
+
+export function serializeAgentFlows(flows: SAgentNode[][]): AgentFlow[] {
+  return flows.map((flow) => serializeAgentFlow(flow));
+}
+
+export function serializeAgentFlow(flow: SAgentNode[]): AgentFlow {
+  return {
+    nodes: flow.map((node) => serializeAgentFlowNode(node)),
+  };
+}
+
+export function serializeAgentFlowNode(node: SAgentNode): AgentFlowNode {
+  return {
+    id: node.id,
+    name: node.data.name,
+    config: serializeAgentFlowNodeConfig(node.data.config),
+    enabled: get(node.data.enabled),
+    x: node.position.x,
+    y: node.position.y,
+    width: node.width,
+    height: node.height,
+  };
+}
+
+function serializeAgentFlowNodeConfig(
+  node_config: Record<string, AgentConfig> | null,
+): Record<string, any> | null {
+  if (node_config === null) {
+    return null;
+  }
+  const config: Record<string, any> = {};
+  for (const key of Object.keys(node_config)) {
+    const t = node_config[key].type;
+    const value = get(node_config[key].value);
+    if (t === "boolean") {
+      config[key] = value === "true";
+    } else if (t === "integer") {
+      config[key] = parseInt(value);
+    } else if (t === "number") {
+      config[key] = parseFloat(value);
+    } else if (t === "string") {
+      config[key] = value;
+    } else if (t === "string?") {
+      config[key] = value === "" ? null : value;
+    } else if (t === "string[]") {
+      config[key] = value.split("\n");
+    } else {
+      config[key] = value;
+    }
+  }
+  return config;
+}
--- src/routes/agents/AgentNode.svelte
+++ src/routes/agents/AgentNode.svelte
@@ -1,82 +1,27 @@
 <script lang="ts">
+  import { get } from "svelte/store";
+  import type { Writable } from "svelte/store";
+
   import { useNodes } from "@xyflow/svelte";
   import type { NodeProps } from "@xyflow/svelte";
   import { Button, Card, Input, Label, NumberInput, Textarea, Toggle } from "flowbite-svelte";
   import { CloseOutline } from "flowbite-svelte-icons";
 
-  import { save_agent_config, set_agent_enabled, start_agent, stop_agent } from "@/lib/utils";
+  import { getAgentSettingsContext } from "@/lib/agent";
+  import type { AgentConfig } from "@/lib/types";
 
   type Props = NodeProps & {
     data: {
       name: string;
-      enabled: boolean;
-      config: Record<string, any>;
-      schema?: Record<string, any>;
-      properties?: Map<string, any>;
+      enabled: Writable<boolean>;
+      config: Record<string, AgentConfig> | null;
     };
   };
 
   let { id, data }: Props = $props();
 
   const nodes = useNodes();
-
-  async function toggle_agent() {
-    if (data.enabled) {
-      data.enabled = false;
-      await set_agent_enabled(id, false);
-      await stop_agent(id);
-    } else {
-      data.enabled = true;
-      await start_agent(id);
-      await set_agent_enabled(id, true);
-    }
-  }
-
-  async function save() {
-    if (data.properties) {
-      const properties = data.properties;
-      let config = { ...data.config };
-      if (properties && config) {
-        for (const [key, value] of properties.entries()) {
-          if (value.type === "boolean") {
-            config = {
-              ...config,
-              [key]: value.value === "true",
-            };
-          } else if (value.type === "integer") {
-            config = {
-              ...config,
-              [key]: parseInt(value.value),
-            };
-          } else if (value.type === "number") {
-            config = {
-              ...config,
-              [key]: parseFloat(value.value),
-            };
-          } else if (value.type === "string") {
-            console.log(config);
-            config = {
-              ...config,
-              [key]: value.value,
-            };
-            console.log(config);
-          } else if (value.type === "string[]") {
-            config = {
-              ...config,
-              [key]: value.value.split("\n"),
-            };
-          } else if (value.type === "string?") {
-            config = {
-              ...config,
-              [key]: value.value === "" ? null : value.value,
-            };
-          }
-        }
-        data.config = config;
-        save_agent_config(data.name, config); // TODO save config in agent flow
-      }
-    }
-  }
+  const agent_schema = getAgentSettingsContext()?.[data.name]?.schema;
 
   function deleteNode() {
     nodes.update((nodes) => {
@@ -87,81 +32,37 @@
 </script>
 
 <div>
-  {#if data.properties}
-    {@const name = data.name}
-    {@const props = data.properties}
-    <Card padding="none">
-      <div class="flex justify-between items-center pl-4 pr-0 mb-2">
-        <h3 class="text-xl pt-2">{data.schema?.["title"] || name}</h3>
-        <Button onclick={deleteNode}><CloseOutline /></Button>
-      </div>
-      <h4 class="text-sm pl-4 pb-4">{data.schema?.["description"] || ""}</h4>
-      <form class="grid grid-cols-6 gap-4 p-4">
-        <Toggle checked={data.enabled as boolean} onchange={() => toggle_agent()} class="col-span-6"
-        ></Toggle>
-        {#each props.keys() as key}
-          {@const prop = props.get(key)}
-          {#if prop}
-            <Label class="col-span-6 space-y-2">
-              <h3>{prop.title || key}</h3>
-              <p class="text-xs text-gray-500">{prop["description"]}</p>
-              {#if prop.type === "boolean"}
-                <Toggle
-                  bind:checked={() => prop.value, (value) => props.set(key, { ...prop, value })}
-                />
-              {:else if prop.type === "integer"}
-                <NumberInput
-                  bind:value={() => prop.value, (value) => props.set(key, { ...prop, value })}
-                />
-              {:else if prop.type === "number"}
-                <Input
-                  type="text"
-                  bind:value={() => prop.value, (value) => props.set(key, { ...prop, value })}
-                />
-              {:else if prop.type === "string" || prop.type === "string?"}
-                <Input
-                  type="text"
-                  bind:value={() => prop.value, (value) => props.set(key, { ...prop, value })}
-                />
-              {:else if prop.type === "string[]"}
-                <Textarea
-                  bind:value={() => prop.value, (value) => props.set(key, { ...prop, value })}
-                  rows={4}
-                />
-              {:else}
-                <Input type="text" value={prop.value} disabled />
-              {/if}
-            </Label>
-          {/if}
+  <Card padding="none">
+    <div class="flex justify-between items-center pl-4 pr-0 mb-2">
+      <h3 class="text-xl pt-2">{agent_schema?.["title"] || data.name}</h3>
+      <Button onclick={deleteNode}><CloseOutline /></Button>
+    </div>
+    <h4 class="text-sm pl-4 pb-4">{agent_schema?.["description"] || ""}</h4>
+    <form class="grid grid-cols-6 gap-4 p-4">
+      <Toggle bind:checked={() => get(data.enabled), (v) => data.enabled.set(v)} class="col-span-6"
+      ></Toggle>
+      {#if data.config}
+        {#each Object.keys(data.config) as key}
+          {@const config = data.config[key]}
+          <Label class="col-span-6 space-y-2">
+            <h3>{config.title || key}</h3>
+            <p class="text-xs text-gray-500">{config["description"]}</p>
+            {#if config.type === "boolean"}
+              <Toggle bind:checked={() => get(config.value), (v) => config.value.set(v)} />
+            {:else if config.type === "integer"}
+              <NumberInput bind:value={() => get(config.value), (v) => config.value.set(v)} />
+            {:else if config.type === "number"}
+              <Input type="text" bind:value={() => get(config.value), (v) => config.value.set(v)} />
+            {:else if config.type === "string" || config.type === "string?"}
+              <Input type="text" bind:value={() => get(config.value), (v) => config.value.set(v)} />
+            {:else if config.type === "string[]"}
+              <Textarea bind:value={() => get(config.value), (v) => config.value.set(v)} rows={4} />
+            {:else}
+              <Input type="text" value={get(config.value)} disabled />
+            {/if}
+          </Label>
         {/each}
-        <Button onclick={() => save()} class="w-fit" outline>Save</Button>
-      </form>
-    </Card>
-  {:else}
-    {@const name = data.name}
-    <Card padding="none">
-      <div class="flex justify-between items-center pl-4 pr-0 mb-2">
-        <h3 class="text-xl pt-2">{name}</h3>
-        <Button onclick={deleteNode}><CloseOutline /></Button>
-      </div>
-      <form class="grid grid-cols-6 gap-4 p-4">
-        <Toggle checked={data.enabled as boolean} onchange={() => toggle_agent()} class="col-span-6"
-        ></Toggle>
-        {#if data.config}
-          {@const config = data.config as Record<string, any>}
-          {#each Object.keys(config) as key}
-            <Label class="col-span-6 space-y-2">
-              <span>{key}</span>
-              {#if typeof config[key] === "boolean"}
-                <Toggle checked={config[key]} disabled />
-              {:else}
-                <Input type="text" value={config[key]} disabled />
-              {/if}
-            </Label>
-          {/each}
-          <Button class="w-fit" outline disabled>Save</Button>
-        {/if}
-      </form>
-    </Card>
-  {/if}
+      {/if}
+    </form>
+  </Card>
 </div>
