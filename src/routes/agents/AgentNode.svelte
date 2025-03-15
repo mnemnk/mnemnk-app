6f90b8aa6458ca37d7efb3672420df772480f364
Akira Ishino <akira@lumilab.jp>
2025-03-15 14:37:29 +0900

    refactor: 💡 define base node for AgentNode and BoardNode

--- src/routes/agents/AgentNode.svelte
+++ src/routes/agents/AgentNode.svelte
@@ -2,13 +2,13 @@
   import { get } from "svelte/store";
   import type { Writable } from "svelte/store";
 
-  import { Handle, Position, useNodes } from "@xyflow/svelte";
   import type { NodeProps } from "@xyflow/svelte";
-  import { Button, Card, Input, Label, NumberInput, Textarea, Toggle } from "flowbite-svelte";
-  import { CloseOutline } from "flowbite-svelte-icons";
+  import { Input, Label, NumberInput, Textarea, Toggle } from "flowbite-svelte";
 
   import { getAgentSettingsContext } from "@/lib/agent";
   import type { AgentConfig } from "@/lib/types";
+
+  import NodeBase from "./NodeBase.svelte";
 
   type Props = NodeProps & {
     data: {
@@ -20,49 +20,39 @@
 
   let { id, data }: Props = $props();
 
-  const nodes = useNodes();
   const agent_schema = getAgentSettingsContext()?.[data.name]?.schema;
-
-  function deleteNode() {
-    nodes.update((nodes) => {
-      nodes = nodes.filter((node) => node.id !== id);
-      return nodes;
-    });
-  }
 </script>
 
-<div>
-  <Handle type="target" position={Position.Left} />
-  <Card padding="none">
-    <div class="flex justify-between items-center pl-4 pr-0 mb-2">
-      <h3 class="text-xl pt-2">{agent_schema?.["title"] ?? data.name}</h3>
-      <Button onclick={deleteNode}><CloseOutline /></Button>
-    </div>
-    <h4 class="text-sm pl-4 pb-4">{agent_schema?.["description"] ?? ""}</h4>
-    <form class="grid grid-cols-6 gap-4 p-4">
-      <Toggle bind:checked={() => get(data.enabled), (v) => data.enabled.set(v)} class="col-span-6"
-      ></Toggle>
-      {#each Object.keys(data.config) as key}
-        {@const config = data.config[key]}
-        <Label class="col-span-6 space-y-2">
-          <h3>{config.title || key}</h3>
-          <p class="text-xs text-gray-500">{config["description"]}</p>
-          {#if config.type === "boolean"}
-            <Toggle bind:checked={() => get(config.value), (v) => config.value.set(v)} />
-          {:else if config.type === "integer"}
-            <NumberInput bind:value={() => get(config.value), (v) => config.value.set(v)} />
-          {:else if config.type === "number"}
-            <Input type="text" bind:value={() => get(config.value), (v) => config.value.set(v)} />
-          {:else if config.type === "string" || config.type === "string?"}
-            <Input type="text" bind:value={() => get(config.value), (v) => config.value.set(v)} />
-          {:else if config.type === "string[]"}
-            <Textarea bind:value={() => get(config.value), (v) => config.value.set(v)} rows={4} />
-          {:else}
-            <Input type="text" value={get(config.value)} disabled />
-          {/if}
-        </Label>
-      {/each}
-    </form>
-  </Card>
-  <Handle type="source" position={Position.Right} />
-</div>
+{#snippet title()}
+  <h3 class="text-xl pt-2">{agent_schema?.["title"] ?? data.name}</h3>
+{/snippet}
+
+{#snippet contents()}
+  <h4 class="text-sm pl-4 pb-4">{agent_schema?.["description"] ?? ""}</h4>
+  <form class="grid grid-cols-6 gap-4 p-4">
+    <Toggle bind:checked={() => get(data.enabled), (v) => data.enabled.set(v)} class="col-span-6"
+    ></Toggle>
+    {#each Object.keys(data.config) as key}
+      {@const config = data.config[key]}
+      <Label class="col-span-6 space-y-2">
+        <h3>{config.title || key}</h3>
+        <p class="text-xs text-gray-500">{config["description"]}</p>
+        {#if config.type === "boolean"}
+          <Toggle bind:checked={() => get(config.value), (v) => config.value.set(v)} />
+        {:else if config.type === "integer"}
+          <NumberInput bind:value={() => get(config.value), (v) => config.value.set(v)} />
+        {:else if config.type === "number"}
+          <Input type="text" bind:value={() => get(config.value), (v) => config.value.set(v)} />
+        {:else if config.type === "string" || config.type === "string?"}
+          <Input type="text" bind:value={() => get(config.value), (v) => config.value.set(v)} />
+        {:else if config.type === "string[]"}
+          <Textarea bind:value={() => get(config.value), (v) => config.value.set(v)} rows={4} />
+        {:else}
+          <Input type="text" value={get(config.value)} disabled />
+        {/if}
+      </Label>
+    {/each}
+  </form>
+{/snippet}
+
+<NodeBase {id} {title} {contents} />
--- src/routes/agents/BoardNode.svelte
+++ src/routes/agents/BoardNode.svelte
@@ -2,12 +2,12 @@
   import { get } from "svelte/store";
   import type { Writable } from "svelte/store";
 
-  import { Handle, Position, useNodes } from "@xyflow/svelte";
   import type { NodeProps } from "@xyflow/svelte";
-  import { Button, Card, Input, Label, Toggle } from "flowbite-svelte";
-  import { CloseOutline } from "flowbite-svelte-icons";
+  import { Input, Label, Toggle } from "flowbite-svelte";
 
   import type { AgentConfig } from "@/lib/types";
+
+  import NodeBase from "./NodeBase.svelte";
 
   const board_name_key = "board_name";
   const persistent_key = "persistent";
@@ -21,47 +21,36 @@
   };
 
   let { id, data }: Props = $props();
-
-  const nodes = useNodes();
-
-  function deleteNode() {
-    nodes.update((nodes) => {
-      nodes = nodes.filter((node) => node.id !== id);
-      return nodes;
-    });
-  }
 </script>
 
-<div>
-  <Handle type="target" position={Position.Left} />
-  <Card padding="none">
-    <div class="flex justify-between items-center pl-4 pr-0 mb-2">
-      <h3 class="text-xl pt-2">{data.name}</h3>
-      <Button onclick={deleteNode}><CloseOutline /></Button>
-    </div>
-    <form class="grid grid-cols-6 gap-4 p-4">
-      <Toggle bind:checked={() => get(data.enabled), (v) => data.enabled.set(v)} class="col-span-6"
-      ></Toggle>
-      <Label class="col-span-6 space-y-2">
-        <h3>Board Name</h3>
-        <Input
-          type="text"
-          bind:value={
-            () => get(data.config[board_name_key].value),
-            (v) => data.config[board_name_key].value.set(v)
-          }
-        />
-      </Label>
-      <Label class="col-span-6 space-y-2">
-        <h3>Persistent</h3>
-        <Toggle
-          bind:checked={
-            () => get(data.config[persistent_key].value),
-            (v) => data.config[persistent_key].value.set(v)
-          }
-        />
-      </Label>
-    </form>
-  </Card>
-  <Handle type="source" position={Position.Right} />
-</div>
+{#snippet title()}
+  <h3 class="text-xl pt-2">{data.name}</h3>
+{/snippet}
+
+{#snippet contents()}
+  <form class="grid grid-cols-6 gap-4 p-4">
+    <Toggle bind:checked={() => get(data.enabled), (v) => data.enabled.set(v)} class="col-span-6"
+    ></Toggle>
+    <Label class="col-span-6 space-y-2">
+      <h3>Board Name</h3>
+      <Input
+        type="text"
+        bind:value={
+          () => get(data.config[board_name_key].value),
+          (v) => data.config[board_name_key].value.set(v)
+        }
+      />
+    </Label>
+    <Label class="col-span-6 space-y-2">
+      <h3>Persistent</h3>
+      <Toggle
+        bind:checked={
+          () => get(data.config[persistent_key].value),
+          (v) => data.config[persistent_key].value.set(v)
+        }
+      />
+    </Label>
+  </form>
+{/snippet}
+
+<NodeBase {id} {title} {contents} />
