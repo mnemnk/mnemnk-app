d388a4e27c395292aacc1ebf3b4917232dd4a66e
Akira Ishino <akira@lumilab.jp>
2025-03-17 00:00:01 +0900

    feat: 🎸 update monitor page (#102)
    * feat: 🎸 update monitor page

--- src/lib/shared.svelte.ts
+++ src/lib/shared.svelte.ts
@@ -5,18 +5,18 @@ import { writable, type Writable } from "svelte/store";
 
 import type { Node } from "@xyflow/svelte";
 
-import type { WriteBoardEmit } from "@/lib/types";
+import type { BoardMessage, WriteBoardEmit } from "@/lib/types";
 
-export const boards = $state(new SvelteMap<string, WriteBoardEmit[]>());
+export const boards = $state(new SvelteMap<string, BoardMessage[]>());
 
 export const nodes: Writable<Node[]> = writable([]);
 
 // same for edges
 export const edges = writable([]);
 
-// const MAX_MESSAGES = 10;
+export const MESSAGES_TIMEOUT = 10 * 60 * 1000;
 
-function addMessage(agent: string, kind: string, value: any) {
+function addMessage(agent: string, kind: string, value: any, time: number) {
   if (!boards.has(kind)) {
     nodes.update((nodes) => {
       return [
@@ -26,25 +26,28 @@ function addMessage(agent: string, kind: string, value: any) {
           type: "board",
           data: { kind },
           position: { x: Math.random() * 1800, y: Math.random() * 1000 },
+          width: 1400,
+          height: 1400,
         },
       ];
     });
   }
   let messages = boards.get(kind) ?? [];
-  messages.push({ agent, kind, value });
-  //   messages = messages.slice(-MAX_MESSAGES);
+  const timeoutThreshold = Date.now() - MESSAGES_TIMEOUT;
+  messages = messages.filter((m) => m.time > timeoutThreshold);
+  messages.push({ agent, kind, value, time });
   boards.set(kind, messages);
 
   nodes.update((nodes) => {
     return [
-      //   ...nodes.slice(-MAX_MESSAGES + 1),
-      ...nodes,
+      ...nodes.filter((n) => !n.data.time || ((n.data.time as number) ?? 0) > timeoutThreshold),
       {
         id: Math.random().toString(),
         type: "message",
-        data: { agent, value },
-        position: { x: Math.random() * 800 - 400, y: Math.random() * 800 - 400 },
+        data: { agent, value, time },
+        position: { x: Math.random() * 1000, y: Math.random() * 1000 },
         parentId: kind,
+        extent: "parent",
       },
     ];
   });
@@ -55,8 +58,8 @@ let unlisten: UnlistenFn | null = null;
 $effect.root(() => {
   listen<WriteBoardEmit>("mnemnk:write_board", (event) => {
     const { agent, kind, value } = event.payload;
-    // console.log("write_board", agent, kind, value);
-    addMessage(agent, kind, value);
+    let time = value.t ?? Date.now();
+    addMessage(agent, kind, value, time);
   }).then((unlistenFn) => {
     unlisten = unlistenFn;
   });
--- src/routes/monitor/MessageNode.svelte
+++ src/routes/monitor/MessageNode.svelte
@@ -1,30 +1,54 @@
 <script lang="ts">
+  import { quadOut } from "svelte/easing";
+
   import type { NodeProps } from "@xyflow/svelte";
-  import { Card, Heading } from "flowbite-svelte";
+
+  import { MESSAGES_TIMEOUT } from "@/lib/shared.svelte";
 
   // extends Props from NodeProps
   type Props = NodeProps & {
     data: {
       agent: string;
-      value: object;
+      value: Record<string, any>;
+      time: number;
     };
   };
 
+  const BACKGROUND = [
+    "bg-gray-200",
+    "bg-gray-200/90",
+    "bg-gray-200/80",
+    "bg-gray-200/70",
+    "bg-gray-200/60",
+    "bg-gray-200/50",
+    "bg-gray-200/40",
+    "bg-gray-200/30",
+    "bg-gray-200/20",
+    "bg-gray-200/10",
+  ];
+
   let { data }: Props = $props();
-  const uid = $props.id();
+
+  let opacity = $state(1.0);
+
+  $effect(() => {
+    const interval = setInterval(() => {
+      opacity = 1.0 - quadOut((Date.now() - data.time) / MESSAGES_TIMEOUT);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
 </script>
 
-<div>
-  <Card class="opacity-90">
-    <Heading tag="h4">
-      {data.agent}
-    </Heading>
-    <div>
-      {#each Object.entries(data.value) as [key, value]}
-        <div>
-          <span>{key}</span>: <span>{value}</span>
-        </div>
-      {/each}
-    </div>
-  </Card>
+<div class="bg-gray-200 max-w-[400px] p-4 rounded-lg" style="opacity: {opacity}">
+  <h3 class="text-lg">{new Date(data.time).toLocaleTimeString()}</h3>
+  {#each Object.entries(data.value) as [key, value]}
+    {#if key !== "t"}
+      <div class="overflow-hidden text-wrap">
+        <span>{key}</span>: <span>{value}</span>
+      </div>
+    {/if}
+  {/each}
 </div>
