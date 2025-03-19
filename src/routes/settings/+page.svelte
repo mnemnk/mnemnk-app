bfab1273462ff6ef10ad30bf89b59d786173aaaf
Akira Ishino <akira@lumilab.jp>
2025-03-19 14:31:42 +0900

    fix: 🐛 remove link to config.yml (#113)

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -5,17 +5,11 @@
   import { Button } from "flowbite-svelte";
 
   import Card from "@/components/Card.svelte";
-  import { get_settings_filepath } from "@/lib/utils";
 
   import Core from "./Core.svelte";
 
   const { data } = $props();
   const settings = $derived(data.settings);
-
-  async function open_settings_file() {
-    const path = await get_settings_filepath();
-    await open(path);
-  }
 
   async function reindex_text() {
     await invoke("reindex_text_cmd");
@@ -23,10 +17,7 @@
 </script>
 
 <main class="container mx-auto p-8 space-y-8 mt-20">
-  <div class="flex">
-    <h1 class="text-xl font-semibold sm:text-2xl">Settings</h1>
-    <Button onclick={open_settings_file} class="ml-auto">config.yml</Button>
-  </div>
+  <h1 class="text-xl font-semibold sm:text-2xl">Settings</h1>
   <Core {settings} />
 
   <Card title="Search">
