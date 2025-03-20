e523f86fa14be1239672c9b530966a785807af31
Akira Ishino <akira@lumilab.jp>
2025-03-20 17:12:15 +0900

    fix: 🐛 remove rebuild search index (#121)

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,26 +1,22 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/core";
-  import { open } from "@tauri-apps/plugin-shell";
-
-  import { Button } from "flowbite-svelte";
-
-  import Card from "@/components/Card.svelte";
-
+  // import { invoke } from "@tauri-apps/api/core";
+  // import { Button } from "flowbite-svelte";
+  // import Card from "@/components/Card.svelte";
   import Core from "./Core.svelte";
 
   const { data } = $props();
   const settings = $derived(data.settings);
 
-  async function reindex_text() {
-    await invoke("reindex_text_cmd");
-  }
+  // async function reindex_text() {
+  //   await invoke("reindex_text_cmd");
+  // }
 </script>
 
 <main class="container mx-auto p-8 space-y-8 mt-20">
   <h1 class="text-xl font-semibold sm:text-2xl">Settings</h1>
   <Core {settings} />
 
-  <Card title="Search">
+  <!-- <Card title="Search">
     <Button onclick={reindex_text} class="w-fit m-2" outline>Reindex Text</Button>
-  </Card>
+  </Card> -->
 </main>
--- src/routes/settings/+page.ts
+++ src/routes/settings/+page.ts
@@ -1,5 +1,3 @@
-import type { Settings } from "$lib/types";
-
 import { get_core_settings } from "@/lib/utils";
 
 export async function load() {
