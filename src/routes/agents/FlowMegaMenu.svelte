c1ac04811348e4540cbd3144d500ebbcf48581d1
Akira Ishino <akira@lumilab.jp>
2025-04-01 13:25:45 +0900

    feat: 🎸 AgentMegaMenu

---
+++ src/routes/agents/FlowMegaMenu.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { MegaMenu } from "flowbite-svelte";
+
+  interface Props {
+    flows: Record<string, any>;
+    flowName: string;
+    open?: boolean;
+  }
+
+  let { flows, flowName = $bindable(), open = $bindable(false) }: Props = $props();
+
+  let flowMenuItems = $derived(
+    Object.keys(flows).map((key) => {
+      return { name: key };
+    }),
+  );
+</script>
+
+<MegaMenu
+  bind:open
+  items={flowMenuItems}
+  let:item
+  class="!bg-gray-100 dark:!bg-gray-900 border-none"
+>
+  <button type="button" onclick={() => (flowName = item.name)} class="m-1">
+    {item.name}
+  </button>
+</MegaMenu>
