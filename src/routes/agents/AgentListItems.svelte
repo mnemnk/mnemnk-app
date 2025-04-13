32a2267bf6fcc39e8f9e9ea3bccccffb979b9b8e
Akira Ishino <akira@lumilab.jp>
2025-04-13 22:32:13 +0900

    style: 💄 agent page (#252)
    ✅ Closes: #251

--- src/routes/agents/AgentListItems.svelte
+++ src/routes/agents/AgentListItems.svelte
@@ -18,21 +18,25 @@
 
 {#each categoryKeys as key}
   {#if key === "00agents"}
-    {#each categories[key] as agentName}
+    {@const agentNames = categories[key].sort()}
+    {#each agentNames as agentName}
       <button
         type="button"
-        class="w-full text-left p-1 hover:bg-gray-200 dark:hover:bg-gray-800 pl-3"
+        class="w-full pl-2 text-left text-gray-400 hover:text-black hover:bg-gray-200 dark:hover:bg-gray-400"
         onclick={() => onAddAgent(agentName)}
       >
         {agentDefs[agentName].title ?? agentName}
       </button>
     {/each}
   {:else}
-    <AccordionItem borderBottomClass="border-b group-last:border-none" paddingFlush="pl-2 py-1">
+    <AccordionItem
+      borderBottomClass="border-b group-last:border-none"
+      paddingFlush="pl-2 pr-2 py-1"
+    >
       <div slot="header">
         {key}
       </div>
-      <Accordion flush class="w-full pr-2 bg-white dark:bg-black">
+      <Accordion flush>
         <AgentListItems categories={categories[key]} {agentDefs} {onAddAgent} />
       </Accordion>
     </AccordionItem>
--- src/routes/agents/NodeContextMenu.svelte
+++ src/routes/agents/NodeContextMenu.svelte
@@ -1,5 +1,6 @@
 <script lang="ts" module>
-  const MENU_CLASS = "w-full text-left pl-2 text-white hover:bg-gray-700";
+  const MENU_CLASS =
+    "w-full text-left pl-2 mb-1 text-gray-400 hover:text-black hover:bg-gray-200 dark:hover:bg-gray-400";
 </script>
 
 <script lang="ts">
@@ -38,11 +39,10 @@
 
 <div
   style="top: {y}px; left: {x}px;"
-  class="absolute p-0 m-0 z-10 text-lg fg-black bg-white dark:fg-white dark:bg-gray-800 border-gray-300 dark:border-gray-700 border-solid border-2 shadow-md"
+  class="absolute w-16 p-0 m-0 z-10 text-lg bg-transparent backdrop-blur-xs border-gray-300 dark:border-gray-800 border-solid border-1 shadow-md"
 >
   <button onclick={handleStart} class={MENU_CLASS}>Start</button>
-  <button onclick={handleStop} class={MENU_CLASS}>Stop</button>
-  <hr class="my-2" />
+  <button onclick={handleStop} class="{MENU_CLASS} border-b border-gray-600 pb-1 mb-1">Stop</button>
   <button onclick={handleCut} class={MENU_CLASS}>Cut</button>
   <button onclick={handleCopy} class="{MENU_CLASS} pb-1">Copy</button>
 </div>
