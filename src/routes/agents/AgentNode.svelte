91f0047c7be55f6698058ffb567f03f2f9093c2b
Akira Ishino <akira@lumilab.jp>
2025-04-14 12:57:36 +0900

    fix: 🐛 agent error message (#258)

--- src/routes/agents/AgentNode.svelte
+++ src/routes/agents/AgentNode.svelte
@@ -98,70 +98,44 @@
 </script>
 
 {#snippet title()}
-  {#if editTitle}
-    <div class="flex-none">
-      <Input
-        class="mt-1"
-        type="text"
-        value={data.title ?? agentDef?.title ?? data.name}
-        onblur={() => (editTitle = false)}
-        onkeydown={(evt) => {
-          if (evt.key === "Enter") {
-            const newTitle = evt.currentTarget.value;
-            if (newTitle === "" || newTitle === (agentDef?.title ?? data.name)) {
-              updateNodeData(id, { title: null });
-            } else if (newTitle !== data.title) {
-              updateNodeData(id, { title: newTitle });
+  <div class="flex-none mt-1">
+    <div class="flex flex-nowrap">
+      {#if editTitle}
+        <Input
+          class="mt-1"
+          type="text"
+          value={data.title ?? agentDef?.title ?? data.name}
+          onblur={() => (editTitle = false)}
+          onkeydown={(evt) => {
+            if (evt.key === "Enter") {
+              const newTitle = evt.currentTarget.value;
+              if (newTitle === "" || newTitle === (agentDef?.title ?? data.name)) {
+                updateNodeData(id, { title: null });
+              } else if (newTitle !== data.title) {
+                updateNodeData(id, { title: newTitle });
+              }
+              editTitle = false;
             }
-            editTitle = false;
-          }
-        }}
-      />
+          }}
+        />
+      {:else}
+        <button
+          id="t-{uid}"
+          type="button"
+          onclick={() => (editTitle = true)}
+          class="flex-none"
+          tabindex={-1}
+        >
+          <h3 class="text-xl">
+            {data.title ?? agentDef?.title ?? data.name}
+          </h3>
+        </button>
+      {/if}
+      {#if errorMessages.length > 0}
+        <ExclamationCircleOutline id="e-{uid}" class="ml-2 pt-1 w-6 h-6 text-red-500" />
+      {/if}
     </div>
-  {:else}
-    <button
-      id="t-{uid}"
-      type="button"
-      onclick={() => (editTitle = true)}
-      class="flex-none mt-1"
-      tabindex={-1}
-    >
-      <h3 class="text-xl">
-        {data.title ?? agentDef?.title ?? data.name}
-      </h3>
-    </button>
-    {#if description || data.title}
-      <Popover triggeredBy="#t-{uid}" placement="top" arrow={false} class="z-40">
-        {#if data.title}
-          <p class="text-sm font-semibold pb-1">{agentDef?.title ?? data.name}</p>
-        {/if}
-        {#if description}
-          <p class="text-sm text-gray-500">{description}</p>
-        {/if}
-      </Popover>
-    {/if}
-    {#if errorMessages.length > 0}
-      <ExclamationCircleOutline id="e-{uid}" class="ml-1 mt-2 w-5 h-5 text-red-500" />
-      <Popover
-        triggeredBy="#e-{uid}"
-        placement="bottom"
-        arrow={false}
-        class="w-96 min-h-60 z-40 text-xs font-light text-gray-500 bg-white dark:bg-gray-800 dark:border-gray-600 dark:text-gray-400 flex flex-col"
-      >
-        <div class="grow flex flex-col gap-2">
-          <Textarea
-            class="grow nodrag nowheel text-wrap"
-            value={errorMessages.join("\n")}
-            onkeydown={(evt) => {
-              evt.preventDefault();
-            }}
-            rows={8}
-          />
-          <Button size="xs" color="red" class="w-10 flex-none" onclick={clearError}>Clear</Button>
-        </div>
-      </Popover>
-    {/if}
-  {/if}
+  </div>
 {/snippet}
 
 {#snippet contents()}
@@ -331,3 +305,35 @@
 {/snippet}
 
 <NodeBase {id} {data} {agentDef} {titleColor} {title} {contents} {...props} />
+
+{#if description || data.title}
+  <Popover triggeredBy="#t-{uid}" placement="top-start" arrow={false} class="z-40">
+    {#if data.title}
+      <p class="text-sm font-semibold pb-1">{agentDef?.title ?? data.name}</p>
+    {/if}
+    {#if description}
+      <p class="text-sm text-gray-500">{description}</p>
+    {/if}
+  </Popover>
+{/if}
+
+{#if errorMessages.length > 0}
+  <Popover
+    triggeredBy="#e-{uid}"
+    placement="bottom"
+    arrow={false}
+    class="w-96 min-h-60 z-40 text-xs font-light text-gray-500 bg-white dark:bg-gray-800 dark:border-gray-600 dark:text-gray-400 flex flex-col"
+  >
+    <div class="grow flex flex-col gap-2">
+      <Textarea
+        class="grow nodrag nowheel text-wrap"
+        value={errorMessages.join("\n")}
+        onkeydown={(evt) => {
+          evt.preventDefault();
+        }}
+        rows={8}
+      />
+      <Button size="xs" color="red" class="w-10 flex-none" onclick={clearError}>Clear</Button>
+    </div>
+  </Popover>
+{/if}
