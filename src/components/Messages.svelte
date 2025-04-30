9b7b4fa82c3148269bcc43ee53cd1fc0146dda94
Akira Ishino <akira@lumilab.jp>
2025-04-30 23:19:18 +0900

    feat: 🎸 support markdown messages (#338)

--- src/components/Messages.svelte
+++ src/components/Messages.svelte
@@ -12,11 +12,35 @@
 </script>
 
 <script lang="ts">
+  import DOMPurify from "dompurify";
   import { Avatar, Card } from "flowbite-svelte";
+  import { marked } from "marked";
 
   let { messages }: Props = $props();
 
-  let msgs = $derived(Array.isArray(messages) ? messages : messages ? [messages] : []);
+  let msgs = $derived.by(() => {
+    let msgArray = Array.isArray(messages) ? messages : messages ? [messages] : [];
+    return msgArray.map((msg) => {
+      if (msg.type === "ai") {
+        if (typeof msg.data.content === "string") {
+          let html = marked.parse(DOMPurify.sanitize(msg.data.content));
+          return { type: msg.type, html };
+        } else if (Array.isArray(msg.data.content)) {
+          let html = marked.parse(DOMPurify.sanitize(msg.data.content.join("\n\n")));
+          return { type: msg.type, html };
+        }
+      } else {
+        if (typeof msg.data.content === "string") {
+          let html = msg.data.content;
+          return { type: msg.type, html };
+        } else if (Array.isArray(msg.data.content)) {
+          let html = msg.data.content.join("\n\n");
+          return { type: msg.type, html };
+        }
+      }
+      return { type: msg.type, html: "" };
+    });
+  });
 </script>
 
 <div class="nodrag nowheel max-h-[800px] overflow-y-auto">
@@ -25,8 +49,11 @@
       <div class="flex items-center space-x-4 rtl:space-x-reverse">
         <Avatar class="flex-none shrink-0">{message.type}</Avatar>
         <div class="grow">
-          <pre class="text-sm text-gray-500 dark:text-gray-400 text-wrap">{message.data
-              ?.content}</pre>
+          {#if message.type === "ai"}
+            {@html message.html}
+          {:else}
+            {message.html}
+          {/if}
         </div>
       </div>
     </Card>
