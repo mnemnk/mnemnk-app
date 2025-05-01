6f975c3cc2269de7c051ee2037c7781b9173fb45
Akira Ishino <akira@lumilab.jp>
2025-05-01 23:22:24 +0900

    feat: 🎸 support array in Get Property agent

--- src-tauri/src/mnemnk/agent/builtins/data.rs
+++ src-tauri/src/mnemnk/agent/builtins/data.rs
@@ -202,22 +202,43 @@ impl AsAgent for GetPropertyAgent {
 
         let props = property.split('.').collect::<Vec<_>>();
 
-        if data.value.is_object() {
+        if data.is_array() {
+            let mut out_arr = Vec::new();
+            for v in data.as_array().context("failed as_array")? {
+                let mut value = v.clone();
+                for prop in &props {
+                    let obj = value.as_object().context("failed as_object2")?;
+                    if let Some(v) = obj.get(*prop) {
+                        value = v.clone();
+                    } else {
+                        value = AgentValue::new_unit();
+                        break;
+                    }
+                }
+                out_arr.push(value);
+            }
+            let kind = if out_arr.is_empty() {
+                "unit"
+            } else {
+                &out_arr[0].kind()
+            };
+            self.try_output(CH_DATA, AgentData::new_array(kind.to_string(), out_arr))
+                .context("Failed to output")?;
+        } else if data.is_object() {
             let mut value = data.value;
             for prop in props {
                 let obj = value.as_object().context("failed as_object")?;
                 if let Some(v) = obj.get(prop) {
                     value = v.clone();
                 } else {
-                    return Ok(());
+                    value = AgentValue::new_unit();
+                    break;
                 }
             }
 
             self.try_output(CH_DATA, AgentData::from_value(value))
                 .context("Failed to output")?;
         }
-
-        // TODO: support array
 
         Ok(())
     }
